c319c528401048a9776158556592f86fe9395ebb
zrq495 <zrq495@gmail.com>
2014-03-24 21:56:49 +0800

    Initial

---
+++ acm_base/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render_to_response
+
+import datetime
+
+def index(request):
+    now = datetime.datetime.now()
+    a = [i for i in range(100)]
+    return render_to_response('index.html', {'now': now, 'a': a})
